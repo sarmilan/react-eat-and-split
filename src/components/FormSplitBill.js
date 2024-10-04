92c60252f4e60f9589c88416dad4403bae95de1a
Milan Somasundaram <sarmilans@gmail.com>
2024-10-04 15:08:40 -0400

    add states to bill calculator, calculate totals

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,21 +1,42 @@
+import { useState } from "react";
 import { Button } from "./Button";
 
 export function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(e) => setBill(Number(e.target.value))}
+        value={bill}
+      />
 
       <label>🧍‍♀️ Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+        value={paidByUser}
+      />
 
       <label>👯‍♀️ {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>💵 Who is paying the bill?</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
